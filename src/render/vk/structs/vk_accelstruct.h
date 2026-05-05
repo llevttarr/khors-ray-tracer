3e2f556db207a1a861d7b8821c515097241a59e4
llevttarr <llevttarr@gmail.com>
2026-05-05 21:52:36 +0300

    accelstruct

---
+++ src/render/vk/structs/vk_accelstruct.h
@@ -0,0 +1,48 @@
+#ifndef VK_ACCELSTRUCT_H
+#define VK_ACCELSTRUCT_H
+
+#include <memory>
+#include <vector>
+#include <functional>
+
+#include "vk_device.h"
+#include "vk_buffer.h"
+#include "scene.h"
+class VKAccelStructure {
+    friend class VKAccelBuilder;
+private:
+    std::shared_ptr<VKDevice>  device;
+    VKBuffer buffer;
+    VkAccelerationStructureKHR handle = VK_NULL_HANDLE;
+    VkDeviceAddress device_address = 0;
+public:
+    VKAccelStructure() = default;
+    ~VKAccelStructure();
+ 
+    VKAccelStructure(const VKAccelStructure&) = delete;
+    VKAccelStructure& operator=(const VKAccelStructure&) = delete;
+    VKAccelStructure(VKAccelStructure&&) noexcept;
+    VKAccelStructure& operator=(VKAccelStructure&&) noexcept;
+ 
+    VkAccelerationStructureKHR get() const { return handle;}
+    VkDeviceAddress get_address() const {return device_address; }
+    bool is_valid() const { return handle != VK_NULL_HANDLE; }
+};
+class VKAccelBuilder {
+public:
+    explicit VKAccelBuilder(std::shared_ptr<VKDevice> device);
+
+    std::unique_ptr<VKAccelStructure> build_blas(const std::vector<RenderTri>& tris,const std::function<void(VkCommandBuffer)>& one_time_submit);
+    std::unique_ptr<VKAccelStructure> build_tlas(const VKAccelStructure& blas,const std::function<void(VkCommandBuffer)>& one_time_submit);
+private:
+    std::shared_ptr<VKDevice> device;
+    VkDeviceSize scratch_alignment() const;
+ 
+    std::unique_ptr<VKAccelStructure> alloc_accel_struct(VkAccelerationStructureTypeKHR type,VkDeviceSize size);
+ 
+    VKBuffer alloc_scratch(VkDeviceSize size);
+};
+ 
+
+
+#endif // VK_ACCELSTRUCT_H
